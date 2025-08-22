19d451129d1ce2cf8fbe640ea0f0eab8c0685380
zongyf <zongyf@trip.com>
2025-08-22 21:50:25 +0800

    refactor: extract MemoOptions to separate types file and remove explicit return type

--- packages/memo/src/index.ts
+++ packages/memo/src/index.ts
@@ -1,25 +1,6 @@
 import React from "react";
 import { debugLog, ignorePropsList } from "./memoHelper";
-
-/**
- * 记忆化组件的配置选项
- *
- * 用于自定义 React.memo 的比较逻辑，提供更精细的性能优化控制。
- *
- * @template P 组件 props 的类型
- */
-interface MemoOptions<P> {
-	/**
-	 * 可选的自定义比较函数
-	 * 如果提供，将使用此函数进行 props 比较
-	 */
-	compare?: (prevProps: Readonly<P>, nextProps: Readonly<P>) => boolean;
-	/**
-	 * 可选的要比较的属性键数组
-	 * 如果指定，将只比较这些属性，忽略其他属性
-	 */
-	propKeys?: Array<keyof P>;
-}
+import type { MemoOptions } from "./types";
 
 /**
  * 创建记忆化组件
@@ -65,7 +46,7 @@ interface MemoOptions<P> {
 function createMemoComponent<P extends object>(
 	Component: React.ComponentType<P>,
 	options?: MemoOptions<P>,
-): React.MemoExoticComponent<React.ComponentType<P>> {
+) {
 	// 如果提供了自定义比较函数，则使用它
 	if (options?.compare) {
 		return React.memo(Component, options.compare);
---
+++ packages/memo/src/types.ts
@@ -0,0 +1,19 @@
+/**
+ * 记忆化组件的配置选项
+ *
+ * 用于自定义 React.memo 的比较逻辑，提供更精细的性能优化控制。
+ *
+ * @template P 组件 props 的类型
+ */
+export interface MemoOptions<P> {
+	/**
+	 * 可选的自定义比较函数
+	 * 如果提供，将使用此函数进行 props 比较
+	 */
+	compare?: (prevProps: Readonly<P>, nextProps: Readonly<P>) => boolean;
+	/**
+	 * 可选的要比较的属性键数组
+	 * 如果指定，将只比较这些属性，忽略其他属性
+	 */
+	propKeys?: Array<keyof P>;
+}
