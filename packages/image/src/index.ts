0fe06caf0d65d1d91b6abb3958288a908ea9ef7b
zongyf <zongyf@trip.com>
2025-09-03 12:35:02 +0800

    feat: 优化 image 包导出方式，添加默认导出并简化命名导出

--- packages/image/src/index.ts
+++ packages/image/src/index.ts
@@ -1,3 +1,6 @@
+import ImageLoader from "./components/ImageLoader";
+import ImagePreloadConsumer from "./components/ImagePreloadConsumer";
+
 // 导出所有 hooks 和组件
 export * from "./types";
 export * from "./hooks/useImagePreload";
@@ -5,5 +8,5 @@ export * from "./utils/preload";
 export * from "./utils/ssr";
 
 // 导出组件
-export { default as ImageLoader } from "./components/ImageLoader";
-export { default as ImagePreloadConsumer } from "./components/ImagePreloadConsumer";
+export { ImageLoader, ImagePreloadConsumer };
+export default ImageLoader;
